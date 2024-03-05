c8bb08dc735478878b7fab568dacb6669ba69f37
Aleksey <meysonsib200189@mail.ru>
2024-03-05 12:46:49 +0700

    Glide - загрузка картинки с интернета

--- app/src/main/java/com/bignerdranch/android/wegoonline/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/wegoonline/MainActivity.java
@@ -2,10 +2,15 @@ package com.bignerdranch.android.wegoonline;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.bumptech.glide.Glide;
 
 import org.json.JSONObject;
 
@@ -19,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    private Button btnActivity;
+    private ImageView imageActivity;
+    private ProgressBar progressBar;
+
     private MainViewModel viewModel;
 
     @Override
@@ -26,14 +35,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         viewModel.loadDogImage();
 
         viewModel.getDogImage().observe(this, new Observer<DogImage>() {
             @Override
             public void onChanged(DogImage dogImage) {
-                Log.d(TAG, dogImage.toString());
+                Glide.with(MainActivity.this)
+                        .load(dogImage.getMessage())//загружаем из ссылки картинку
+                        .into(imageActivity);       //указываем где отобразить картинку
             }
         });
     }
+
+    private void initViews() {
+        btnActivity = findViewById(R.id.btnActivity);
+        imageActivity = findViewById(R.id.imageActivity);
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+
 }
